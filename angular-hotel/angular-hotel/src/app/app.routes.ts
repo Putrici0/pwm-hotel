e1476494b921a5270f80460bd6f854affb0d236f
paull <paullopisparrilla@gmail.com>
2026-04-07 18:52:23 +0100

    index completed in angular with components not finished

--- angular-hotel/angular-hotel/src/app/app.routes.ts
+++ angular-hotel/angular-hotel/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BookingComponent } from './pages/booking/booking.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { RestaurantComponent } from './pages/restaurant/restaurant.component';
+import { WellnessFacilitiesComponent } from './pages/wellness-facilities/wellness-facilities.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LegalComponent } from './pages/legal/legal.component';
+import { accountGuard, adminGuard } from './guards/auth.guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'restaurant', component: RestaurantComponent },
+  { path: 'wellness-facilities', component: WellnessFacilitiesComponent },
+  { path: 'activities', component: ActivitiesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'account', component: AccountComponent, canActivate: [accountGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
+  { path: '**', redirectTo: '' }
+];
