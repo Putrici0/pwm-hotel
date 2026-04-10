ed61ff17606f23629b7a85f90dada533ed840558
Joan <drdoffensmirhtz@gmail.com>
2026-04-10 18:40:14 +0100

    feat: implement authentication, admin data management, and Firebase integration services

--- angular-hotel/angular-hotel/src/app/services/site-data.service.ts
+++ angular-hotel/angular-hotel/src/app/services/site-data.service.ts
@@ -1,24 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { map, Observable, shareReplay } from 'rxjs';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiteDataService {
-  private readonly http = inject(HttpClient);
-  private data$?: Observable<Record<string, unknown>>;
-
-  getAllData(): Observable<Record<string, unknown>> {
-    if (!this.data$) {
-      this.data$ = this.http
-        .get<Record<string, unknown>>('/data/site-data.json')
-        .pipe(shareReplay(1));
-    }
-    return this.data$;
-  }
+  private readonly firestore = inject(Firestore);
 
   getSection<T>(sectionKey: string): Observable<T> {
-    return this.getAllData().pipe(map((data) => data[sectionKey] as T));
+    const docRef = doc(this.firestore, `siteData`, sectionKey);
+    return docData(docRef) as Observable<T>;
   }
 }
---
+++ angular-hotel/angular-hotel/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZtS78FeAjEpXDs0Z2aVOd5Gy_tEfzhbg",
+    authDomain: "pwm-hotel.firebaseapp.com",
+    projectId: "pwm-hotel",
+    storageBucket: "pwm-hotel.firebasestorage.app",
+    messagingSenderId: "1020668559325",
+    appId: "1:1020668559325:web:07fdd2a035a2eaf9114034"
+  }
+};
